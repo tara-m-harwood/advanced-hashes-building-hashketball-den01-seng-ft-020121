3e3abf651f1e86b6790f623cb5e5790a8aa3e9d3
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-24 21:20:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ def game_hash
                   :brook_lopez => { :player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15 },
                   :mason_plumlee => { :player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5 },
                   :jason_terry => { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 }
-              }
+              },
     :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],
               :players => [
                  {}
