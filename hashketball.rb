b7b6513972ebcc941290c7429159ecf9b05939cf
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-24 20:53:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,7 +16,7 @@ def game_hash
               :players => [
                  {:jeff_adrien => { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 }},
                  {:bismack_biyombo => { :player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10 }},
-                 {
+                 {:desagna_diop =>
                 ]
     }
   }
