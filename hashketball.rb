576ef64cbd267c0a67ad052a426c8355c9697074
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-24 20:42:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,8 +5,9 @@ def game_hash
     :home => {:team_name => "Brooklyn Nets", :colors => ["Black, White"],
               :players => [
                   {:alan_anderson => { :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1 }},
-                  {:reggie_evans =>  { :player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7 }},
-                  {:brook_lopez => 
+                  {:reggie_evans => { :player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7 }},
+                  {:brook_lopez => { :player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15 }},
+                  
                   ]
                 
     },
