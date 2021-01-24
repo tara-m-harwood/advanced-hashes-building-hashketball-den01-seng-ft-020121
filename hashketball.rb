23f92f5124900089c7b7225ff15f03bf92cb4131
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-24 20:52:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,8 @@ def game_hash
     :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],
               :players => [
                  {:jeff_adrien => { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 }},
-                 {:bismack_biyombo => 
+                 {:bismack_biyombo => { :player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10 }},
+                 {
                 ]
     }
   }
