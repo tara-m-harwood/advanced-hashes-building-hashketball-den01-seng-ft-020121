192e60a325cd18bfd14fdf431b036caa97e3d654
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-24 21:34:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,8 @@
 def game_hash
   game_hash = {
     :home => {:team_name => "Brooklyn Nets", :colors => ["Black, White"],
-              :players => [ {:alan_anderson => { :player_name => "Alan Anderson"}} ]
+              :players => [ { :player_name => "Alan Anderson"} ] 
+              ]
               },
     :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],
               :players => [
